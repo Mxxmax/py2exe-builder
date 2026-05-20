bdc57a4311d27f052d43f9f888950bd0ec6c4410
Mxxmax <103110524+Mxxmax@users.noreply.github.com>
2026-05-21 01:12:28 +0800

    cleanup old test file

--- hello.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Test script: hello world with argparse."""
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--name", default="世界")
-args = parser.parse_args()
-print(f"你好, {args.name}!")
-input("按 Enter 键退出...")
